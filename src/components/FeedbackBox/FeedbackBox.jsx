1a73a2df0a772b957f8163236e05675ec2e531b8
Chay Carnell <chay.carnell@ibm.com>
2021-03-12 20:47:43 +0000

    deployment

--- src/components/FeedbackBox/FeedbackBox.jsx
+++ src/components/FeedbackBox/FeedbackBox.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import { RichText, Text } from "../../components";
 import * as S from "./FeedbackBox.styles";
 
+// Set some rich text options
+const textOptions = {
+  pSize: "14px",
+  bSize: "14px",
+};
+
 const Render = ({ feedback = [] }) => (
   <S.FeedbackWrapper>
     {feedback.map((item) => (
       <S.FeedbackCard key={item.sys.id}>
-        <RichText document={item.fields.feedback} />
-        <Text type="bold">{item.fields.role}</Text>
+        <RichText document={item.fields.feedback} textOptions={textOptions} />
+        <Text type="bold" size={"14px"}>
+          {item.fields.role}
+        </Text>
       </S.FeedbackCard>
     ))}
   </S.FeedbackWrapper>
