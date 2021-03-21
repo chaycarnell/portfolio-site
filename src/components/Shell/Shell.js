32ac2650d5d698ec50528c3575deeb2740c4c2bf
Chay Carnell <chay.carnell@ibm.com>
2021-03-21 16:16:11 +0000

    deployment

--- src/components/Shell/Shell.js
+++ src/components/Shell/Shell.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 const Shell = styled.div`
   display: grid;
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
   grid-auto-flow: ${({ viewport }) =>
     viewport === "desktop" ? "column" : "row"};
   grid-auto-rows: ${({ viewport }) =>
