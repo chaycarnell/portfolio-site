9cb86db23f205b12d681f1e3b5eaa0de45411ea2
Chay Carnell <chaycarnell@gmail.com>
2024-02-23 19:42:16 +1000

    fix(app): update app import in index to reflect casing change

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import ReactGA from 'react-ga4';
 
-import App from './app';
+import App from './App';
 
 ReactGA.initialize('G-YM8FR742SY');
 
