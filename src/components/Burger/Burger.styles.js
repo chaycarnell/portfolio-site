32338f749c1c7ff13f14044795a23399a70ca238
Chay Carnell <chaycarnell@gmail.com>
2024-02-27 16:27:37 +1000

    fix(animation): fix burger button animation

--- src/components/Burger/Burger.styles.js
+++ src/components/Burger/Burger.styles.js
@@ -15,17 +15,17 @@ export const Burger = styled.button`
     height: 0.25rem;
     background: var(--white);
     transition: all 0.3s linear;
-    transform-origin: 1px;
-    :first-child {
-      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
-    }
-    :nth-child(2) {
-      opacity: ${({ open }) => (open ? "0" : "1")};
-      transform: ${({ open }) => (open ? "translateX(20px)" : "translateX(0)")};
-    }
-    :nth-child(3) {
-      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
-    }
+    transform-origin: 2px;
+  }
+  :first-child {
+    transform: ${({ $open }) => ($open ? "rotate(45deg)" : "rotate(0)")};
+  }
+  :nth-child(2) {
+    opacity: ${({ $open }) => ($open ? "0" : "1")};
+    transform: ${({ $open }) => ($open ? "translateX(20px)" : "translateX(0)")};
+  }
+  :nth-child(3) {
+    transform: ${({ $open }) => ($open ? "rotate(-45deg)" : "rotate(0)")};
   }
   :focus {
     outline: none;
